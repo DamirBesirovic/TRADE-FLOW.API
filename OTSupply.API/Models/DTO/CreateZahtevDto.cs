1a7a94cd09ada55480eb6f1a5c8f7d957bcacffa
MedzidJasarovic <medzidjasarevic1212@gmail.com>
2025-09-07 23:56:01 +0200

    nothing much

--- OTSupply.API/Models/DTO/CreateZahtevDto.cs
+++ OTSupply.API/Models/DTO/CreateZahtevDto.cs
@@ -3,7 +3,7 @@
     public class CreateZahtevDto
     {
         public Guid OglasId { get; set; }     
-        public Guid GradId { get; set; }       
+        public Guid GradId { get; set; }       //za dropdown
         public string Poruka { get; set; }     
         public string Telefon { get; set; }   
         public int Kolicina { get; set; }      
